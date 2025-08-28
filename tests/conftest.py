b26ae1e0a182aa50ef170de7eaa72dd558471f67
Matthew Evans <git@ml-evs.science>
2025-08-28 14:38:51 +0100

    Skip client auth tests if no creds provided

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,8 @@ from icsd_optimade.client import ICSDClient
 
 @pytest.fixture()
 def icsd_credentials():
-    return os.getenv("ICSD_LOGIN_ID") and os.getenv("ICSD_LOGIN_PASSWORD")
+    if not (os.getenv("ICSD_LOGIN_ID") and os.getenv("ICSD_LOGIN_PASSWORD")):
+        pytest.skip("No ICSD credentials set.")
 
 
 @pytest.fixture()
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,15 +1,6 @@
-import pytest
-
-
 def test_login_credentials(icsd_credentials, icsd_client):
-    if not icsd_credentials:
-        pytest.skip("No ICSD credentials set.")
-
     assert icsd_client.login()
 
 
 def test_date_range(icsd_credentials, icsd_client):
-    if not icsd_credentials:
-        pytest.skip("No ICSD credentials set.")
-
     assert icsd_client.query_date_range((2020, 2021))
