e58813a0643a65e64fe223bbdd724da3b482b588
Matthew Evans <git@ml-evs.science>
2025-12-22 14:46:42 +0000

    Add lots of functionality to client: CIF caching, session refreshing on rate limiting etc.

--- src/icsd_optimade/client.py
+++ src/icsd_optimade/client.py
@@ -1,12 +1,25 @@
 import datetime
 import logging
 import os
+import random
+import time
 from pathlib import Path
 from typing import Literal
 
 import httpx
 
 from icsd_optimade import __version__
+
+log = logging.getLogger("client")
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(
+    logging.Formatter("%(asctime)s - [PID: %(process)d] - %(levelname)s - %(message)s")
+)
+log.addHandler(console_handler)
+
+
+class Forbidden(RuntimeError): ...
 
 
 class ICSDClient:
@@ -20,20 +33,29 @@ class ICSDClient:
     _headers: dict[str, str] = {}
     _timeout: httpx.Timeout = httpx.Timeout(10.0, read=60.0)
 
+    exponential_retries: int = 7
+    """Whether to retry CIF downloads when rate-limited; each retry
+    will add another power of 10 to the back-off time, starting at 60s.
+    """
+
     def __init__(self):
         self._http_client = httpx.Client
         # Check for `ICSD_LOGIN_ID` and `ICSD_LOGIN_PASSWORD` environment variables
         self.icsd_login_id = os.getenv("ICSD_LOGIN_ID")
         self.icsd_login_password = os.getenv("ICSD_LOGIN_PASSWORD")
-        self.log = logging.getLogger("icsd_client")
         self._cached_token_path = Path(".icsd-auth-token")
+        self._limit_reached: bool = False
+        self._ctime = datetime.datetime.now()
+        self._cifs_downloaded: int = 0
 
         if not self.icsd_login_id or not self.icsd_login_password:
             raise RuntimeError(
                 "No ICSD user credentials found, please set the `ICSD_LOGIN_ID` and `ICSD_LOGIN_PASSWORD` environment variables."
             )
 
-        self.headers["User-Agent"] = f"icsd-optimade ingester/{__version__}"
+        self.headers["User-Agent"] = (
+            f"icsd-optimade ingester/{__version__} Contact dev@datalab.industries with issues/problematic usage"
+        )
         auth_token = self.login()
         self.headers["icsd-auth-token"] = auth_token
 
@@ -49,7 +71,38 @@ class ICSDClient:
             raise RuntimeError(
                 f"Failed to logout of ICSD session: {logout_resp.content}"
             )
-        self.log.info("Logged out of session")
+        log.error("Logged out of session")
+
+    def rotate_session(self):
+        """Attempt to rotate the session key in a parallel-safe way."""
+        owns_lock = False
+        lock = Path(".icsd-session.lock")
+        time.sleep(random.random() * 5)
+        if not lock.is_file():
+            with open(lock, "w") as f:
+                f.write(str(os.getpid()))
+
+            owns_lock = True
+
+        if owns_lock:
+            log.error("Rotating ICSD session key")
+            # One PID can now clear the session and create a new one
+            self.logout()
+            if self._cached_token_path.is_file():
+                self._cached_token_path.unlink()
+
+            self.login()
+            lock.unlink()
+
+        if not owns_lock:
+            log.error("Waiting for new session")
+            # Wait a bit for another process to finish locking and creating the session
+            max_wait_seconds = 120
+            wait_seconds = 0
+            increment = 1
+            while lock.is_file() and wait_seconds < max_wait_seconds:
+                time.sleep(increment)
+                wait_seconds += increment
 
     def login(self) -> str:
         """Login with user credentials and return the ICSD auth token."""
@@ -57,13 +110,13 @@ class ICSDClient:
         # Check for pre-existing sessions
         auth_token = os.getenv("ICSD_AUTH_TOKEN")
         if auth_token:
-            self.log.debug(f"Trying pre-existing session from env var {auth_token}")
+            log.debug(f"Trying pre-existing session from env var {auth_token}")
 
         else:
             if self._cached_token_path.is_file():
                 with open(self._cached_token_path) as f:
                     auth_token = f.readlines()[0].strip()
-            self.log.debug(
+            log.debug(
                 f"Trying pre-existing session from path {self._cached_token_path}"
             )
 
@@ -73,7 +126,7 @@ class ICSDClient:
                 f"{self.base_url}/cif/0", headers={"icsd-auth-token": auth_token}
             )
             if check_auth.status_code == 404:
-                self.log.info(f"Using pre-existing login session {auth_token}")
+                log.error(f"Using pre-existing login session {auth_token}")
                 return auth_token
 
         login_resp = httpx.post(
@@ -85,9 +138,9 @@ class ICSDClient:
             raise RuntimeError(
                 f"Failed to authenticate to ICSD at {self.base_url!r}: {login_resp.status_code=}. Error returned: {login_resp.content}"
             )
-        with open(".icsd-auth-token", "w") as f:
+        with open(self._cached_token_path, "w") as f:
             f.write(login_resp.headers["icsd-auth-token"])
-        self.log.info(f"Logged into session {login_resp.headers['icsd-auth-token']}")
+        log.error(f"Logged into session {login_resp.headers['icsd-auth-token']}")
         return login_resp.headers["icsd-auth-token"]
 
     @property
@@ -108,14 +161,34 @@ class ICSDClient:
 
     def get_cif(self, identifier: str | int) -> bytes:
         """Download a CIF for the entry given the ICSD identifier and return it as bytes."""
-        self.headers.pop("Accept")
-        response = self.session.get(f"{self.base_url}/cif/{identifier}")
-        if response.status_code != 200:
-            raise RuntimeError(
-                f"CIF {identifier} not found: [{response.status_code}] - {response.content}"
-            )
+        if self._limit_reached:
+            raise Forbidden("CIF download limit reached, try again later.")
 
-        return response.content
+        self.headers.pop("Accept", None)
+
+        retries = 0
+        if self.exponential_retries:
+            while retries < self.exponential_retries:
+                response = self.session.get(f"{self.base_url}/cif/{identifier}")
+
+                if response.status_code == 403:
+                    sleep_time = 60 * 2**retries
+                    log.error(
+                        f"Hit 403 limit after {datetime.datetime.now() - self._ctime} on process {os.getpid()} -- {self._cifs_downloaded} CIFs downloaded. Process sleeping for %s minutes before retrying",
+                        sleep_time / 60,
+                    )
+                    time.sleep(sleep_time)
+                    self.rotate_session()
+                    retries += 1
+                    continue
+
+                self._cifs_downloaded += 1
+                return response.content
+
+        self._limit_reached = True
+        raise Forbidden(
+            "CIF download limit reached: [{response.status_code}] - {response.content}"
+        )
 
     def get_reference(self, identifier: str) -> str:
         """Download the bibliographic references for the entry given the ICSD identifier."""
@@ -126,7 +199,7 @@ class ICSDClient:
         self.headers["Accept"] = "application/json"
         resp = self.session.get(f"{self.base_url}/search/expert?query={query}")
         if resp.status_code != 200:
-            raise RuntimeError(f"Search returned an error: {resp.content}")
+            raise RuntimeError(f"Search {query} returned an error: {resp.content}")
 
         return resp.json()["idnums"]
 
--- tests/test_client.py
+++ tests/test_client.py
@@ -18,4 +18,15 @@ def test_recording_date_range(icsd_credentials, icsd_client):
     results = icsd_client.query_by_date_range(
         (date_2, date_1), date_field="recordingdate"
     )
-    assert len(results) == 10030
+    assert len(results) == 10_030
+    results = icsd_client.query_by_date_range(
+        (date_1, date_2), date_field="recordingdate"
+    )
+    assert len(results) == 10_030
+
+    date_1 = datetime.date(year=1900, month=1, day=1)
+    date_2 = datetime.date(year=1990, month=1, day=1)
+    results = icsd_client.query_by_date_range(
+        (date_2, date_1), date_field="recordingdate"
+    )
+    assert len(results) == 25_495
